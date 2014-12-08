486028f515988c743f47ceb978308d627a0a16ee
Raj Perera <rajiteh@gmail.com>
2014-12-08 13:26:41 -0500

    Route listing, stop listing and presets

--- checkbus.sh
+++ checkbus.sh
@@ -2,29 +2,98 @@
 
 # What stop to watch.
 AGENCY="ttc"
-ROUTE=$1
-STOP=$2
+BASE_URL="http://webservices.nextbus.com/service/publicXMLFeed"
 
 # Times, in minutes, for different colors in the display.
-MIN_BLUE_TIME=38
-MIN_GREEN_TIME=28
-MIN_YELLOW_TIME=21
+MIN_BLUE_TIME=20
+MIN_GREEN_TIME=10
+MIN_YELLOW_TIME=5
 
 INCLUDE_CURRENT_DATE=true
-
 UPDATE_INTERVAL_SEC=30
 
-URL="http://webservices.nextbus.com/service/publicXMLFeed?"
-URL+="command=predictions&a=${AGENCY}&r=${ROUTE}&s=${STOP}"
-#echo $URL
 COLOR_BLUE="\x1b[34;1m"
 COLOR_GREEN="\x1b[32;1m"
 COLOR_YELLOW="\x1b[33;1m"
 COLOR_NONE="\x1b[0m"
 
+#Check for some env var and set $1 and $2 gitignored due to privacy
+if [ -a "presets.sh" ]; then
+  source presets.sh
+fi
+
+#Helpers
+resolve_route_names() {
+  local routesURL="${BASE_URL}?command=routeList&a=${AGENCY}"
+  local result=`curl -s "$routesURL" | grep '^<route.*tag=.*>$'`
+  local route_names
+  local route_ids
+  IFS="####" read -a route_names <<<  `echo "${result}" | \
+					grep -o "title="'"[^"]*"' | \
+					sed "s/^title=//" | \
+					sed 's/"//g' | \
+					tr '\n' '####'`
+  IFS="####" read -a route_ids <<<  `echo "${result}" | \
+					grep -o "tag="'"[^"]*"' | \
+					sed "s/^tag=//" | \
+					sed 's/"//g' | \
+					tr '\n' '####'`
+  echo "Listing routes for ${AGENCY}"
+  for i in "${!route_ids[@]}"
+  do
+    echo "[${route_ids[i]}] ${route_names[i]}"
+  done
+  echo -n "Type in the value inside [] for your route:"
+  return `read retval && echo $retval`
+}
+
+resolve_stop_names() {
+  local route=$1
+  local stopsURL="${BASE_URL}?command=routeConfig&a=${AGENCY}&r=${route}"
+  local result=`curl -s "$stopsURL" | grep '^<stop.*tag=.*>$'`
+  local title_names
+  local stop_ids
+  IFS="####" read -a title_names <<<  `echo "${result}" | \
+					grep -o "title="'"[^"]*"' | \
+					sed "s/^title=//" | \
+					sed 's/"//g' | \
+					tr '\n' '####'`
+  IFS="####" read -a stop_ids <<<  `echo "${result}" | \
+					grep -o "tag="'"[^"]*"' | \
+					sed "s/^tag=//" | \
+					sed 's/"//g' | \
+					tr '\n' '####'`
+  echo "Listing stops for ${route}"
+  for i in "${!stop_ids[@]}"
+  do
+    echo "[${stop_ids[i]}] ${title_names[i]}"
+  done
+  echo -p "Type in the value inside [] for your stop:"
+  return `read retval && echo $retval`
+}
+
+#Figure out route and stop
+if [ -z "$1" ]; then
+  resolve_route_names
+  route=$?
+else
+  route=$1
+fi
+if [ -z "$2" ]; then
+  resolve_stop_names $route
+  stop=$?
+else
+  stop=$2
+fi
+
+#Proceed
+clear
+url="${BASE_URL}?command=predictions&a=${AGENCY}&r=${route}&s=${stop}"
 while true; do
  ($INCLUDE_CURRENT_DATE && date +"%A %F %I:%M %P"
-  curl -s "$URL" | \
+  echo "$route"
+  result=`curl -s "$url"`
+  echo $result | \
     grep 'prediction epochTime' | \
     grep -o "minutes="'"[^"]*"' | \
     sed "s/^minutes=//" | \
@@ -40,5 +109,5 @@ while true; do
     done) | \
     tr '\n' '\t'
   echo
-  sleep 30
+  sleep $UPDATE_INTERVAL_SEC
 done
